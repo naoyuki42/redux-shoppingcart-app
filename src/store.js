bd6198cf7c105a346e21257268a5f774d800a7f0
naoyuki42 <naoyuki.7042@outlook.jp>
2022-11-24 22:17:32 +0900

    fix: ファイル整理

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./features/cart/CartSlice";
-
-export const store = configureStore({
-  reducer: {
-    cart: cartSlice,
-  },
-});
