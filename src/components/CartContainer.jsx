95e9f5c5851dfcc68ea7e8c54863665a68a50921
naoyuki42 <naoyuki.7042@outlook.jp>
2022-11-28 22:08:25 +0900

    add: cart-container

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from "react";
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
+
+const CartContainer = React.memo(() => {
+  const { amount, cartItems, total } = useSelector((state) => state.cart);
+  const clearCart = useCallback(() => {}, []);
+  return (
+    <section className="cart">
+      <header>
+        <h2>買い物かご</h2>
+      </header>
+      {amount < 1 ? (
+        <p className="empty-cart">何も入ってません…</p>
+      ) : (
+        <>
+          <div>
+            {cartItems.map((item) => {
+              return <CartItem key={item.id} />;
+            })}
+          </div>
+          <footer>
+            <hr />
+            <div className="cart-total">
+              <h4>
+                合計：<span>{total}円</span>
+              </h4>
+            </div>
+            <button className="btn clear-btn" onClick={clearCart()}>
+              全削除
+            </button>
+          </footer>
+        </>
+      )}
+    </section>
+  );
+});
+
+export default CartContainer;
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartItem = React.memo(() => {
+  return <div>CartItem</div>;
+});
+
+export default CartItem;
